200c6d44df7c0b68e357ef4c74a712cbcaabee59
rajgop1 <rajgopaljakhmola1@gmail.com>
2024-07-06 14:28:42 +0530

    initial commit

---
+++ src/app/components/home/landing.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Landing() {
+  return (
+    <section>
+        <section className=''></section>
+        <section className=''></section>
+    </section>
+  )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import Landing from "./components/home/landing";
+
+export default function Home() {
+  return (
+    <main>
+      <Landing/>
+    </main>
+  );
+}

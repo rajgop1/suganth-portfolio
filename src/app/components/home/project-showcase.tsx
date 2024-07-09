276b3ccd92b6b022c10131e9af36c485f1959afd
rajgop1 <rajgopaljakhmola1@gmail.com>
2024-07-09 13:49:45 +0530

    update: bento box

--- src/app/components/home/project-showcase.tsx
+++ src/app/components/home/project-showcase.tsx
@@ -1,5 +1,7 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
+import style from "./project-showcase.module.css"
 
 const CARD_CLASS = 'px-6 py-4 border border-gray-100 rounded-[20px] text-white'
 
@@ -32,21 +34,66 @@ export default function ProjectShowcase() {
                     </div>
                 </div>
             </div>
+            <div className={`${CARD_CLASS} flex flex-col justify-between`}>
+                <div className='flex flex-col gap-2'>
+                    <div className='text-3xl font-bold'>Me?</div>
+                    <p className='text-xl'>
+                        <span className='text-gray-300'>
+                            I’m passion about creating {' '}
+                        </span>
+                        emotional experiences {' '}
+                        <span className='text-gray-300'>
+                            through digital {' '}
+                        </span>
+                        design, {' '}
+                        <span className='text-gray-300'>
+                            and I'm interested in {' '}
+                        </span>
+                        Artificial Intelligence, Virtual Reality.</p>
+                </div>
+                <div className='flex flex-row gap-20 justify-between items-end'>
+                    <Image src="/assets/images/gamepad-big.png" alt='Gamepad Big' width={140} height={117} className='' />
+                    <div className='flex flex-col gap-[8px]'>
+                        <div className='text-2xl'>Connect with me</div>
+                        <div className='flex flex-row gap-2 justify-end'>
+                            <Link href="/" className='bg-black-200 border border-gray-400 p-[8px] flex flex-row justify-center items-center rounded-full h-[35px] w-[35px]'>
+                                <Image src="/assets/images/gmail.png" alt='Gmail' width={200} height={200} className='w-full h-full object-contain' />
+                            </Link>
+                            <Link href="/" className='bg-black-200 border border-gray-400 p-[8px] flex flex-row justify-center items-center rounded-full h-[35px] w-[35px]'>
+                                <Image src="/assets/images/instagram.png" alt='Instagram' width={200} height={200} className='w-full h-full object-contain' />
+                            </Link>
+                            <Link href="/" className='bg-black-200 border border-gray-400 p-[8px] flex flex-row justify-center items-center rounded-full h-[35px] w-[35px]'>
+                                <Image src="/assets/images/linkedin.png" alt='Linkedin' width={200} height={200} className='w-full h-full object-contain' />
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <div className={`${CARD_CLASS}`}>
-                <div className='text-2xl'>Me?</div>
-                <p className=''>
-                    <span className='text-gray-300'>
-                        I’m passion about creating
-                    </span>
-                    emotional experiences
-                    <span className='text-gray-300'>
-                        through digital
-                    </span>
-                    design,
-                    <span className='text-gray-300'>
-                        and I'm interested in
-                    </span>
-                    Artificial Intelligence, Virtual Reality.</p>
+                <Image src="/assets/images/my-photo-big.png" alt='My Profile Image' width={340} height={450} className='w-full h-full object-cover' />
+            </div>
+            <div className='flex flex-col gap-4'>
+                <div className={`${CARD_CLASS} flex flex-col py-8`}>
+                    <div className='flex flex-row justify-center'>
+                        <div className='text-center'>
+                            <span className='text-green-100 line-through'>
+                                <span className='text-gray-500 text-2xl font-semibold'>2023</span>
+                            </span>
+                            <span className='text-3xl font-semibold'>{' '} 2024</span>
+                        </div>
+                    </div>
+                    <div className='text-lg font-semibold text-center tracking-[0.25rem] uppercase'>Expert - Design Trend </div>
+                </div>
+                <div className={`${CARD_CLASS} flex-1`}>
+                    <div className={`flex flex-col gap-2 justify-end bg-[url("/assets/images/bento-box.png")] bg-cover bg-no-repeat h-full rounded-[10px] ${style.bentoBoxBg}`}>
+                        <span className='px-4 text-2xl font-bold'>
+                            Bento Box
+                        </span>
+                        <span className='px-4 pb-2 text-sm font-semibold'>
+                            Grid-based approach, where content is organised into distinct 'cells' that work together to create aesthetically pleasing user interfaces and a strong content hierarchy
+                        </span>
+                    </div>
+                </div>
             </div>
         </section>
     )
